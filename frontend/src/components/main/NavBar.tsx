8666c3feb3a89b9bc161b4ad6d755d3983685370
lalakely <lalakely@rakotoherilala04@gmail.com>
2024-11-17 22:26:57 +0300

    Better view for this part

--- frontend/src/components/main/NavBar.tsx
+++ frontend/src/components/main/NavBar.tsx
@@ -1,10 +1,23 @@
 import { Link } from "react-router-dom";
 
-function NavBar(){
-    return <>
-        <Link to="/">Enfants</Link>
-        <Link to="/Vaccins">Vaccins</Link>
-    </>
+function NavBar() {
+    return (
+        <div className="fixed top-0 left-0 h-full bg-blue-600 w-48 flex flex-col items-start p-4 text-white">
+            <h2 className="text-xl font-bold mb-6">Menu</h2>
+            <Link 
+                to="/" 
+                className="py-2 px-4 mb-2 w-full text-left hover:text-white hover:bg-blue-700 rounded text-white"
+            >
+                Enfants
+            </Link>
+            <Link 
+                to="/Vaccins" 
+                className="py-2 px-4 w-full text-left hover:bg-blue-700 hover:text-white rounded text-white"
+            >
+                Vaccins
+            </Link>
+        </div>
+    );
 }
 
 export default NavBar;
