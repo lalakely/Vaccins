cca5c8aaa7716ad5ef93672857e32aaa5589fe94
lalakely <lalakely@rakotoherilala04@gmail.com>
2024-11-18 23:40:58 +0300

    Added the path for the route for Vaccine

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('./config/db');
 const cors = require('cors');
 const enfantsRoutes = require('./routes/enfantRoutes');
+const vaccinsRoutes = require('./routes/vaccinRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,8 @@ app.use(express.json());
 
 // Utilisation du routes
 app.use('/api', enfantsRoutes);
+app.use('/api', vaccinsRoutes);
+
 
 app.listen( PORT , () => {
     console.log('Server is runing on http://localhost');
