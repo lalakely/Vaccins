c2cf2ada01014a366ddc14e663337d4273ce5d56
lalakely <rakotoherilala04@gmail.com>
2024-12-11 19:24:07 +0300

    First design of the list of fokotany

--- frontend/src/components/Fokotany/FokotanyList.tsx
+++ frontend/src/components/Fokotany/FokotanyList.tsx
@@ -1,8 +1,52 @@
+import axios from "axios";
+import { useEffect , useState } from "react";
+import FokotanyCard from "./FokotanyCard";
+import FokotanyPopup from "./FokotanyPopup";
+
 function FokotanyList (){
+    const [data , setData] = useState([]);
+    const [selectedFokotany , setSelectedFokotany] = useState([]);
+    const [showPopup , setShowPopup] = useState(false);
+
+
+    axios.get("http://localhost:3000/api/fokotany")
+        .then((response) => {
+            setData(response.data);
+        })
+        .catch((error) => {
+            console.log("Error fetching data :" , error);
+        });
+    ;
+
+    const handleDetailsClick = (fokotany) => {
+        setSelectedFokotany(fokotany);
+        setShowPopup(true);
+    }
+
+    const closePopup = () => {
+        setShowPopup(false);
+        setSelectedFokotany(null);
+    }
+
     return (
-        <>
-            Voici la liste des fokotany
-        </>
+        <div className="p-20">     
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                {data.map((fokotany) => (
+                    <FokotanyCard 
+                        key={fokotany.id}
+                        fokotany={fokotany}
+                        onDetailsClick={handleDetailsClick}
+                    />
+                ))}
+            </div>
+            {
+                showPopup && (
+                    <FokotanyPopup 
+                        fokotany={selectedFokotany}
+                        onClose={closePopup}
+                    />
+            )}
+        </div>
     );
 }
 
