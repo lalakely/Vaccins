b6536318d89d63acb2b5173abeb5e1f9f7f5e869
lalakely <rakotoherilala04@gmail.com>
2024-12-11 19:23:07 +0300

    First design of the Cards for fokotany

--- frontend/src/components/Fokotany/FokotanyCard.tsx
+++ frontend/src/components/Fokotany/FokotanyCard.tsx
@@ -1,5 +1,21 @@
-function FokotanyCard (){
+function FokotanyCard ({fokotany , onDetailsClick}){
+    return(
+        <div
+            key={fokotany.id}
+            className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition-shadow"
+        >
+            <h2 className="text-2xl font-semibold text-gray-800 mb-6 border-b pb-2">
+                {fokotany.Nom}
+            </h2>
+            <button
+                onClick={() => onDetailsClick(fokotany)}
+                className="text-blue-500"
+            >
+                Détails
+            </button>
+        </div>
 
+    );
 }
 
 export default FokotanyCard;
