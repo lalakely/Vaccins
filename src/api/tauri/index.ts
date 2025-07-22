ec1bbb3d18676e6ed1bbe1e6b141a276eae280b1
lalakely <rakotoherilala04@gmail.com>
2025-07-22 23:28:38 +0300

    chore: remove Tauri API files and update build paths in tauri.conf.json

--- src/api/tauri/index.ts
+++
@@ -1,9 +0,0 @@
-// Exporte toutes les API Tauri pour la base de données locale
-export * from './database';
-export * from './userApi';
-export * from './enfantApi';
-export * from './vaccinApi';
-export * from './vaccinationApi';
-export * from './fokotanyApi';
-export * from './hameauApi';
-export * from './notificationApi';
